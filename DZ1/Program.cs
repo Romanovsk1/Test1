f848dbf81079b91ce6e6c2b0fc2dff370fa27463
Romanovsk1 <93801993+Romanovsk1@users.noreply.github.com>
2022-02-08 22:56:55 +0400

    test _commit

---
+++ DZ1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace DZ1
+{
+    internal class Program
+    {
+         
+         
+        
+        static void Main()
+        {
+           
+            Console.WriteLine($"Здравствуйте, как вас зовут? "); // Приветствия + вопрос о имени
+            string name = Console.ReadLine(); //Ввод пользователем имени
+            Console.WriteLine($"Привет, {name} сегодня " + DateTime.Now); //Вывод имени + нынешней даты)
+         
+        }
+    }
+}
